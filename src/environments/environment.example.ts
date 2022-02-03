315f395070c730674a4010162a4fdae2b232f7fc
gracg <grac@capite.de>
2022-02-02 21:16:43 -0500

    Initial Commit

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,9 @@
+/*
+  This file is to show the structure of environment.ts and environment.prod.ts
+  Whenever the project is cloned, environment.ts and environment.prod.ts must be created for a proper build.
+*/
+
+export const environment = {
+  production: false,
+  giphyApiKey: "123"
+};
