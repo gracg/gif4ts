a070d59528be422a028d5fe7aa2084f02ca4d32c
gracg <grac@capite.de>
2022-02-03 03:44:21 -0500

    New topbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { MaincompComponent } from './components/maincomp/maincomp.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MaincompComponent
+    MaincompComponent,
+    TopbarComponent
   ],
   imports: [
     BrowserModule,
